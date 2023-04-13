be0b727c487a2f91322e07b2c84347cc5a8173e4
thiagodasilvadebortoli <129125327+thiagodasilvadebortoli@users.noreply.github.com>
2023-04-13 20:23:15 -0300

    Add files via upload

---
+++ curso/estruturaa/projeto/js/script.js
@@ -0,0 +1,9 @@
+function alterarh1(msg){
+    document.getElementById('msg').innerHTML = msg;
+
+}
+function alterarcortamanho() {
+    document.getElementById('msg').style.color = "#B7d5e5"
+    document.getElementById('msg').style.fontsize = "100px"
+    
+}
